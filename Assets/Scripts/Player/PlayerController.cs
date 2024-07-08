3a5d226d741823ae2364325063415611e6368527
only1hay <only1hay@nate.com>
2024-07-08 19:08:16 +0900

    [Add] 2단 점프

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,22 +3,55 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    //public InputActionAsset inputActionAsset;
+    public InputActionAsset inputActionAsset;
+    private InputAction moveAction;
+    private InputAction jumpAction;
+    private InputAction skillAction;
+    private Rigidbody2D rigid;
 
     public float moveSpeed = 7f;
-    public float jumpForce = 7f;
+    public float jumpForce = 13f;
     private Vector2 inputVector;
     private Vector3 moveVector;
 
     public Transform groundCheck; //플레이어 발위치 체크
     public LayerMask groundLayer;
-
-    private Rigidbody2D rigid;
     private bool isGrounded;
+    private int jumpCount = 0;
+
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+
+        var playerActionMap = inputActionAsset.FindActionMap("PlayerActions");
+        moveAction = playerActionMap.FindAction("Move");
+        jumpAction = playerActionMap.FindAction("Jump");
+        skillAction = playerActionMap.FindAction("Skill");
+    }
+
+    private void OnEnable()
+    {
+        moveAction.Enable();
+        jumpAction.Enable();
+        skillAction.Enable();
+
+        moveAction.performed += OnMove;
+        moveAction.canceled += OnMove;
+        jumpAction.performed += OnJump;
+        skillAction.performed += OnSkill;
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+        jumpAction.Disable();
+        skillAction.Disable();
+
+        moveAction.performed -= OnMove;
+        moveAction.canceled -= OnMove;
+        jumpAction.performed -= OnJump;
+        skillAction.performed -= OnSkill;
     }
 
     void Start()
@@ -27,6 +60,14 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer); // 발이 땅에 닿아있는지 체크
+        if (isGrounded)
+        {
+            jumpCount = 0; // 바닥에 닿으면 jumpCount 리셋
+        }
+    }
 
     void Update()
     {
@@ -35,19 +76,28 @@ public class PlayerController : MonoBehaviour
 
     public void OnMove(InputAction.CallbackContext value)
     {
-        // velocity->1초당 움직이는 거리
         //rigid.velocity = new Vector2(moveInput.x * moveSpeed, rigid.velocity.y);
         inputVector = value.ReadValue<Vector2>();
-        moveVector = new Vector3(inputVector.x, 0f, inputVector.y);
-
+        moveVector = new Vector3(inputVector.x, 0f, 0f);
     }
 
     public void OnJump(InputAction.CallbackContext value)
     {
         if (value.performed)
         {
-            rigid.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-
+            if (isGrounded) //바닥이거나
+            {
+                //rigid.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                rigid.velocity = new Vector2(rigid.velocity.x, jumpForce);
+                jumpCount = 1;
+            }
+            else if (jumpCount < 2) // jumpCount가 2회미만일때만 점프 가능
+            {
+                rigid.velocity = new Vector2(rigid.velocity.x, 0f); // y 축 속도 초기화 -> 일정한 높이의 점프
+                //rigid.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                rigid.velocity = new Vector2(rigid.velocity.x, jumpForce);
+                jumpCount = 2;
+            }
         }
     }
 
