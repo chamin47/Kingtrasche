acf71ad1f616fdd759027d82202743a63b5b1578
chamin47 <152050575+chamin47@users.noreply.github.com>
2024-07-09 20:09:57 +0900

    [Add] 보스 투사체 생성

--- Assets/Scripts/BossMonster/BossBattleController.cs
+++ Assets/Scripts/BossMonster/BossBattleController.cs
@@ -11,25 +11,37 @@ public class BossBattleController : MonoBehaviour
     public Transform theBoss;
     public float bossGrowSpeed = 2f;
 
-    // Start is called before the first frame update
+    public Transform projectileLauncher;
+    public float launcherGrowSpeed = 2f;
+
+    public float launcherRotateSpeed = 90f;
+    private float launcherRotation;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (bossActive == true)
         {
-            if(theBoss.localScale != Vector3.one)
+            if (theBoss.localScale != Vector3.one)
             {
                 theBoss.localScale = Vector3.MoveTowards(
                     theBoss.localScale,
                     Vector3.one,
                     bossGrowSpeed * Time.deltaTime);
             }
-        }
+
+            if (projectileLauncher.transform.localScale != Vector3.one)
+            {
+                projectileLauncher.localScale = Vector3.MoveTowards(
+                    projectileLauncher.localScale,
+                    Vector3.one,
+                    launcherGrowSpeed * Time.deltaTime);
+            }
+        }        
     }
 
     public void ActivateBattle()
