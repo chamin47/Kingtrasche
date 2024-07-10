de73dd1d1f1e0dd58559e4a5a8a48da6e48eaffb
chamin47 <152050575+chamin47@users.noreply.github.com>
2024-07-10 18:35:53 +0900

    [None] Scene 매니저 작업 중 다른 브런치 탐색

---
+++ Assets/Scripts/Scenes/TitleScene.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScene : BaseScene
+{
+	protected override void Init()
+	{
+		base.Init();
+
+		SceneType = Define.Scene.Title;
+
+		// Managers.UI.ShowSceneUI<UI_Inven>();
+	}
+
+	public override void Clear()
+	{
+		
+	}
+}
