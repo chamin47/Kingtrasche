5d391623d0bdc14dbc9a2434d70bf530e26256b0
chamin47 <152050575+chamin47@users.noreply.github.com>
2024-07-15 20:51:24 +0900

    [Add] 맵데이터 구글시트 연동

--- Assets/Scripts/Managers/RunningMapManager.cs
+++ Assets/Scripts/Managers/RunningMapManager.cs
@@ -1,22 +1,22 @@
+using GameBalance;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RunningMapManager : MonoBehaviour
-{
-    // [todo] 엑셀연동해서 자동으로 스테이지별 맵경로 배열 가져오기
-    private string[][] stageChunk =
-    {
-       new string[] { "MapChunk/TestChunk001", "MapChunk/TestChunk003", "MapChunk/TestChunk005", "MapChunk/TestChunk002" },
-       new string[] {"MapChunk/TestChunk006","MapChunk/TestChunk007","MapChunk/TestChunk001" }
-    };
+{    
+    MapChunkData chunkData;
 
     private float chunkSpace = 18f; // 맵 간 간격
-    public int currentStage = 1; // [todo] 각 스테이지넘버를 누르면 currentStage 넘버 변경
+    private int currentStage; // [todo] 각 스테이지넘버를 누르면 currentStage 넘버 변경
 
-    void Start()
+	void Start()
     {
-        string[] prefabPaths = stageChunk[currentStage - 1]; //스테이지별 mapChunk배열주소 가져옴
-        GameObject[] mapChunk = new GameObject[prefabPaths.Length];
-        for (int i = 0; i < prefabPaths.Length; i++)
+        currentStage = SceneManager.GetActiveScene().buildIndex;
+		chunkData = MapChunkData.MapChunkDataMap[currentStage];
+		List<string> prefabPaths = chunkData.MapChunksPath;
+		GameObject[] mapChunk = new GameObject[prefabPaths.Count];
+        for (int i = 0; i < prefabPaths.Count; i++)
         {
             mapChunk[i] = Managers.Resource.Load<GameObject>(prefabPaths[i]);
         }
