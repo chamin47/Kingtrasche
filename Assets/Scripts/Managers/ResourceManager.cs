ae80463571a80d21b721f9802f9d75f8ce639899
only1hay <only1hay@nate.com>
2024-07-15 17:53:25 +0900

    [Fix] 맵 경로 수정

--- Assets/Scripts/Managers/ResourceManager.cs
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -1,36 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ResourceManager
 {
-	public T Load<T>(string path) where T : Object
-	{
-		return Resources.Load<T>(path);
-	}
+    public T Load<T>(string path) where T : Object
+    {
+        return Resources.Load<T>($"Prefabs/{path}");
+    }
 
-	public GameObject Instantiate(string path, Transform parent = null)
-	{
-		GameObject prefab = Load<GameObject>($"Prefabs/{path}");
-		if (prefab == null)
-		{
-			Debug.Log($"Failed to load prefab : {path}");
-			return null;
-		}
+    public GameObject Instantiate(string path, Transform parent = null)
+    {
+        GameObject prefab = Load<GameObject>($"Prefabs/{path}");
+        if (prefab == null)
+        {
+            Debug.Log($"Failed to load prefab : {path}");
+            return null;
+        }
 
-		GameObject go = Object.Instantiate(prefab, parent);
-		int index = go.name.IndexOf("(Clone)");
-		if (index > 0)
-			go.name = go.name.Substring(0, index);
+        GameObject go = Object.Instantiate(prefab, parent);
+        int index = go.name.IndexOf("(Clone)");
+        if (index > 0)
+            go.name = go.name.Substring(0, index);
 
-		return go;
-	}
+        return go;
+    }
 
-	public void Destroy(GameObject go)
-	{
-		if (go == null)
-			return;
+    public void Destroy(GameObject go)
+    {
+        if (go == null)
+            return;
 
-		Object.Destroy(go);
-	}
+        Object.Destroy(go);
+    }
 }
