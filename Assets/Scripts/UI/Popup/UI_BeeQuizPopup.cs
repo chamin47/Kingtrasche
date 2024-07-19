d9d9474ea67cf4ace2703436771b25ae48faf1e2
only1hay <only1hay@nate.com>
2024-07-19 23:29:22 +0900

    [Add] 벌퀴즈 선택지

--- Assets/Scripts/UI/Popup/UI_BeeQuizPopup.cs
+++ Assets/Scripts/UI/Popup/UI_BeeQuizPopup.cs
@@ -7,14 +7,14 @@ public class UI_BeeQuizPopup : UI_Popup
 {
     enum Buttons
     {
-        SecletButton01,
-        SecletButton02
+        SelectButton01,
+        SelectButton02
     }
 
     enum Texts
     {
-        SecletButton01Text,
-        SecletButton02Text
+        SelectButton01Text,
+        SelectButton02Text
     }
 
     public Action OnEndEvent;
@@ -31,8 +31,8 @@ public class UI_BeeQuizPopup : UI_Popup
         Bind<TMP_Text>(typeof(Texts));
         Bind<Button>(typeof(Buttons));
 
-        GetButton((int)Buttons.SecletButton01).gameObject.BindEvent(OnClickSelcctButton);
-        GetButton((int)Buttons.SecletButton02).gameObject.BindEvent(OnClickSelcctButton);
+        GetButton((int)Buttons.SelectButton01).gameObject.BindEvent(OnClickSelcctButton);
+        GetButton((int)Buttons.SelectButton02).gameObject.BindEvent(OnClickSelcctButton);
 
 
         return true;
@@ -40,7 +40,37 @@ public class UI_BeeQuizPopup : UI_Popup
 
     public void OnClickSelcctButton(PointerEventData eventData)
     {
-        OnEndEvent.Invoke();
+        OnEndEvent?.Invoke();
         Destroy(this.gameObject);
+    }
+
+    public void SetCorrectOrIncorrectBeeNumber(int correctNumber)
+    {
+        TMP_Text text01 = GetText((int)Texts.SelectButton01Text);
+        TMP_Text text02 = GetText((int)Texts.SelectButton02Text);
+        string mari = " ¸¶¸®";
+
+        int incorrectNumber;
+        incorrectNumber = UnityEngine.Random.Range(5, 10);
+
+        if (incorrectNumber == correctNumber)
+        {
+            while (correctNumber != incorrectNumber)
+            {
+                incorrectNumber = UnityEngine.Random.Range(5, 10);
+            }
+        }
+
+        int temp = UnityEngine.Random.Range(1, 11);
+        if (temp <= 5)
+        {
+            text01.text = correctNumber.ToString() + mari;
+            text02.text = incorrectNumber.ToString() + mari;
+        }
+        else
+        {
+            text01.text = incorrectNumber.ToString() + mari;
+            text02.text = correctNumber.ToString() + mari;
+        }
     }
 }
