ed14e711d1ea51f0a16d68040f56caf8e91a0caf
only1hay <only1hay@nate.com>
2024-07-22 00:59:56 +0900

    [Fix] 클리어버튼

--- Assets/Scripts/Tutorial/TutorialHouse.cs
+++ Assets/Scripts/Tutorial/TutorialHouse.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class TutorialHouse : MonoBehaviour
 {
-    public GameObject SecondDescription;
+    public GameObject TutorialUI;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             other.gameObject.SetActive(false);
-            SecondDescription.SetActive(false);
+            TutorialUI.SetActive(false);
             GameObject TutorialClear = Managers.Resource.Load<GameObject>("UI/Popup/UI_TutorialClearPopup");
             Instantiate(TutorialClear);
         }
