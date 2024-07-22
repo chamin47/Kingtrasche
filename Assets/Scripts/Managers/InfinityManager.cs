a302c694e80b48a26348a5bec408c456cc99e430
only1hay <only1hay@nate.com>
2024-07-23 03:15:58 +0900

    [Add]무한모드 구현

--- Assets/Scripts/Managers/InfinityManager.cs
+++ Assets/Scripts/Managers/InfinityManager.cs
@@ -1,53 +1,96 @@
-using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class InfinityManager : MonoBehaviour
 {
+    public Transform Player;
     private GameObject[] mapChunk;
+    private List<GameObject> activeChunkList = new List<GameObject>(); // 활성 청크 리스트
+    private List<GameObject> deactiveChunkList = new List<GameObject>(); //비활성 청크 리스트
 
-    private int activeChunk; // 10개 미만이면 다시 배치
+    private int activeChunkNumber; // 10개 미만이면 다시 배치
     private int chunkSpace = 18;
-    private float deactiiveTime = 5f;
+    private float deactiveDistance = -36f;
     private Vector3 lastChunkVector;
 
+    private void Awake()
+    {
+        mapChunk = Resources.LoadAll<GameObject>("Prefabs/MapChunk/");
+    }
 
     private void Start()
     {
-        mapChunk = Resources.LoadAll<GameObject>("Prefabs/MapChunk/");
-        Shuffle(mapChunk);
-        GameObject[] SuffleChunk = new GameObject[mapChunk.Length];
-        for (int i = 0; i < mapChunk.Length; i++)
+        List<GameObject> mapChunkList = new List<GameObject>(mapChunk);
+        Shuffle(mapChunkList);
+        for (int i = 0; i < mapChunkList.Count; i++)
         {
             Vector3 chunkPosition = new Vector3(chunkSpace + i * chunkSpace, 0, 0);
-            GameObject go = Instantiate(mapChunk[i], chunkPosition, Quaternion.identity);
+            GameObject go = Instantiate(mapChunkList[i], chunkPosition, Quaternion.identity);
             go.SetActive(true);
-            activeChunk++;
+
+            activeChunkList.Add(go); // 활성청크 리스트에 추가 (처음 모든 맵이 들어감)
+            activeChunkNumber++;
+            lastChunkVector = chunkPosition; // 마지막 청크의 포지션으로 계속 업뎃
         }
     }
 
     private void Update()
     {
-        if (activeChunk <= 10)
-        {
+        GoThroughPlayer();
 
+        if (activeChunkNumber <= 10)
+        {
+            SetNewMap();
         }
     }
 
-    IEnumerator DeactiveChunk(float time)
+    private void GoThroughPlayer() // 플레이어가 지나가면 뒤에 있는 맵이 비활성화
     {
-        yield return new WaitForSeconds(time);
-        activeChunk--;
+        for (int i = 0; i < activeChunkList.Count; i++)
+        {
+            GameObject go = activeChunkList[i];
+            float gapX = go.transform.position.x - Player.position.x;
+            if (gapX < deactiveDistance)
+            {
+                DeactiveChunk(go);
+            }
+        }
     }
 
-    private void Shuffle(GameObject[] array)
+    private void SetNewMap() //비활성화 된 청크만 다시 배치
     {
-        for (int i = 0; i < array.Length; i++)
+        Shuffle(deactiveChunkList);
+
+        foreach (var chunk in deactiveChunkList)
         {
-            GameObject temp = array[i];
-            int randomIndex = Random.Range(0, array.Length);
-            array[i] = array[randomIndex];
-            array[randomIndex] = temp;
+            Vector3 newChunkPosition = lastChunkVector + new Vector3(chunkSpace, 0, 0);
+            chunk.transform.position = newChunkPosition;
+            chunk.SetActive(true);
+
+            activeChunkList.Add(chunk);
+            activeChunkNumber++;
+            lastChunkVector = newChunkPosition;
+        }
+        deactiveChunkList.Clear();
+    }
+
+    private void DeactiveChunk(GameObject chunk)
+    {
+        activeChunkList.Remove(chunk);
+        chunk.SetActive(false);
+        deactiveChunkList.Add(chunk);
+        activeChunkNumber--;
+    }
+
+    private void Shuffle(List<GameObject> chunk)
+    {
+        for (int i = 0; i < chunk.Count; i++)
+        {
+            GameObject temp = chunk[i];
+            int randomIndex = Random.Range(0, chunk.Count);
+            chunk[i] = chunk[randomIndex];
+            chunk[randomIndex] = temp;
         }
     }
 }
