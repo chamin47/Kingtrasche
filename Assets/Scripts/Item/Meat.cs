28bd2d6ff4f195aa2e687cbf9db7409936c15ced
only1hay <only1hay@nate.com>
2024-07-23 20:08:14 +0900

    [Add] 고기갯수 UI연동

--- Assets/Scripts/Item/Meat.cs
+++ Assets/Scripts/Item/Meat.cs
@@ -2,22 +2,11 @@ using UnityEngine;
 
 public class Meat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
+            UI_GameScene.AddScore();
             Destroy(this.gameObject);
         }
     }
--- Assets/Scripts/UI/Scene/UI_GameScene.cs
+++ Assets/Scripts/UI/Scene/UI_GameScene.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -7,44 +5,54 @@ using UnityEngine.UI;
 
 public class UI_GameScene : UI_Scene
 {
-	#region enum
-	enum GameObjects
-	{
-	}
+    #region enum
+    enum GameObjects
+    {
+    }
 
-	enum Buttons
-	{
-		PauseButton
-	}
+    enum Buttons
+    {
+        PauseButton
+    }
 
-	enum Texts
-	{
+    enum Texts
+    {
+        CurrentMeatText
+    }
 
-	}
+    enum Images
+    {
 
-	enum Images
-	{
+    }
+    #endregion
 
-	}
-	#endregion
+    private static int currentScore;
+    private static TMP_Text currentMeat;
 
-	public override bool Init()
-	{
-		if (base.Init() == false)
-			return false;
+    public override bool Init()
+    {
+        if (base.Init() == false)
+            return false;
 
-		Bind<TMP_Text>(typeof(Texts));
-		Bind<Button>(typeof(Buttons));
-		Bind<Image>(typeof(Images));
+        Bind<TMP_Text>(typeof(Texts));
+        Bind<Button>(typeof(Buttons));
+        Bind<Image>(typeof(Images));
 
-		Get<Button>((int)Buttons.PauseButton).gameObject.BindEvent(OnClickPauseButton);
+        Get<Button>((int)Buttons.PauseButton).gameObject.BindEvent(OnClickPauseButton);
+        currentMeat = GetText((int)Texts.CurrentMeatText);
 
-		return true;
-	}
+        return true;
+    }
 
-	private void OnClickPauseButton(PointerEventData eventData)
-	{
-		Managers.UI.ShowPopupUI<UI_PausePopup>();
-		Time.timeScale = 0;
-	}
+    private void OnClickPauseButton(PointerEventData eventData)
+    {
+        Managers.UI.ShowPopupUI<UI_PausePopup>();
+        Time.timeScale = 0;
+    }
+
+    public static void AddScore()
+    {
+        currentScore += 10;
+        currentMeat.text = currentScore.ToString();
+    }
 }
