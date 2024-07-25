27c8b25ed0a3c7f56539f2713cff0d832b48102f
only1hay <only1hay@nate.com>
2024-07-25 17:38:21 +0900

    [Fix] RunningScene연동

--- Assets/Scripts/Managers/SceneManagerEx.cs
+++ Assets/Scripts/Managers/SceneManagerEx.cs
@@ -1,48 +1,47 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneManagerEx
 {
-	private Dictionary<Scene, string> sceneNameMap = new Dictionary<Scene, string>()
-	{
-		{ Scene.Unknown, "UnknownScene" },
-		{ Scene.Title, "Title Scene" },
-		{ Scene.StageSelect, "StageScene" },
-		{ Scene.Game, "HAY Scene" },
-		{ Scene.Boss, "BossScene1" },
-		{ Scene.Boss2, "BossScene2" },
-		{ Scene.Infinity, "InfinityRunningScene" },
-		{ Scene.Tutorial, "RunningTutorialScene" }
-	};
+    private Dictionary<Scene, string> sceneNameMap = new Dictionary<Scene, string>()
+    {
+        { Scene.Unknown, "UnknownScene" },
+        { Scene.Title, "Title Scene" },
+        { Scene.StageSelect, "StageScene" },
+        { Scene.Game, "RunningScene" },
+        { Scene.Boss, "BossScene1" },
+        { Scene.Boss2, "BossScene2" },
+        { Scene.Infinity, "InfinityRunningScene" },
+        { Scene.Tutorial, "RunningTutorialScene" }
+    };
 
-	public BaseScene CurrentScene { get { return GameObject.FindAnyObjectByType<BaseScene>(); } }  
+    public BaseScene CurrentScene { get { return GameObject.FindAnyObjectByType<BaseScene>(); } }
 
     public void LoadScene(Scene type)
     {
-		Managers.Clear();
+        Managers.Clear();
 
         SceneManager.LoadScene(GetSceneName(type));
     }
 
-	string GetSceneName(Scene type)
-	{
-		if (sceneNameMap.TryGetValue(type, out string name))
-		{
-			return name;
-		}
-		return null;
-	}
+    string GetSceneName(Scene type)
+    {
+        if (sceneNameMap.TryGetValue(type, out string name))
+        {
+            return name;
+        }
+        return null;
+    }
 
-	public void Clear()
-	{
-		CurrentScene?.Clear();
-	}
+    public void Clear()
+    {
+        CurrentScene?.Clear();
+    }
 
-	//string GetSceneName(Define.Scene type)
- //   {
- //       string name = System.Enum.GetName(typeof(Define.Scene), type);
- //       return name;
- //   }
+    //string GetSceneName(Define.Scene type)
+    //   {
+    //       string name = System.Enum.GetName(typeof(Define.Scene), type);
+    //       return name;
+    //   }
 }
