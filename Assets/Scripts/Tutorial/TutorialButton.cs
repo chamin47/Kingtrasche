7d71123a7a7a3591de5421e410f1ed2cb865955a
only1hay <only1hay@nate.com>
2024-07-25 19:50:30 +0900

    [Add] 진척도 UI

--- Assets/Scripts/Tutorial/TutorialButton.cs
+++ Assets/Scripts/Tutorial/TutorialButton.cs
@@ -5,6 +5,7 @@ public class TutorialButton : MonoBehaviour
 
     public void OnClickClearButton()
     {
+        Managers.Sound.Play("switch10", Sound.Effect);
         Managers.Scene.LoadScene(Scene.StageScene);
     }
 }
