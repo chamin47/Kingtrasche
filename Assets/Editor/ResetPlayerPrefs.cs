3756817e613d418ba2e6299796380bb8c922f0b3
only1hay <only1hay@nate.com>
2024-08-09 16:09:10 +0900

    [Fix] 오타수정, 데이터시트 업뎃

--- Assets/Editor/ResetPlayerPrefs.cs
+++ Assets/Editor/ResetPlayerPrefs.cs
@@ -6,7 +6,7 @@ public class ResetPlayerPrefs : MonoBehaviour
     [MenuItem("Window/PlayerPrefs 초기화")]
     private static void ResetPrefs()
     {
-        if (EditorUtility.DisplayDialog("게임 세이브 정보 삭제", "정말 살제 하시겠습니까?", "네", "아니오"))
+        if (EditorUtility.DisplayDialog("게임 세이브 정보 삭제", "정말 삭제 하시겠습니까?", "네", "아니오"))
         {
             Debug.Log("삭제 완료");
             PlayerPrefs.DeleteAll();
